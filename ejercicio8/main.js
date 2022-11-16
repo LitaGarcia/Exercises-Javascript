5abaf45fabbc62a18824f36a99109b968a05485b
Manuela Garcia <manuela.garcia@bosonit.com>
2022-11-16 17:08:40 +0100

    done ex 10

--- ejercicio8/main.js
+++ ejercicio8/main.js
@@ -1,15 +1,20 @@
+function fromBytesToFormattedSizeUnits(a, digits = 3) {
+  const values = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+  const bytes = Math.abs(a);
+  if (bytes == 0) return "0 Byte";
+  const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1000)));
+  const result = (bytes / Math.pow(1000, i)).toPrecision(digits) + " " + values[i];
+  if (a < 0) {
+    return "-" + result;
+  } 
+  return result;
+}
 
+const result1 = fromBytesToFormattedSizeUnits(1000);
+console.log(result1); // 1KB
 
+const result2 = fromBytesToFormattedSizeUnits(123456789);
+console.log(result2); // 123MB
 
-
-
-
-
-const result = fromBytesToFormattedSizeUnits(1000);
-console.log(result); // 1KB
- 
-const result = fromBytesToFormattedSizeUnits(123456789);
-console.log(result); // 123MB
- 
-const result = fromBytesToFormattedSizeUnits(-12145489451.5932, 5);
-console.log(result); // -12.145GB
+const result3 = fromBytesToFormattedSizeUnits(-12145489451.5932, 5);
+console.log(result3); // -12.145GB
