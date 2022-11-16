3ab922cf9a0e9c62b1482794a62d0ea80add0142
Manuela Garcia <manuela.garcia@bosonit.com>
2022-11-16 13:02:22 +0100

    done ex 9 and 10

---
+++ ejercicio10/main.js
@@ -0,0 +1,9 @@
+
+function removeHTMLTags (str) {
+return str.replace(/<[^>]*>/g, '')
+}
+
+
+const result = removeHTMLTags('<div><span>lorem</span> <strong>ipsum</strong></div>');
+ 
+console.log(result); // lorem ipsum
---
+++ ejercicio9/main.js
@@ -0,0 +1,13 @@
+
+
+function toLowercaseKeys (object) {
+    const x = {}
+    for(let key in object){
+        x[key.toLowerCase()] = object[key]
+       }
+    return x
+    }
+
+const myObject = { NamE: 'Charles', ADDress: 'Home Street' };
+const myObjLowercase = toLowercaseKeys(myObject);
+console.log(myObjLowercase); // { name: 'Charles', address: 'Home Street' }
